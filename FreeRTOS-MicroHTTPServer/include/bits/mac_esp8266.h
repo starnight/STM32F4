4c2bef75fedcfe9b9e8f16f28b417e1354ac0eb1
StarNight <zack_pan@alumni.ncu.edu.tw>
2016-05-24 23:17:09 +0800

    Micro HTTP Server on FreeRTOS draft.

--- FreeRTOS-MicroHTTPServer/include/bits/mac_esp8266.h
+++ FreeRTOS-MicroHTTPServer/include/bits/mac_esp8266.h
@@ -1,5 +1,7 @@
-#ifndef __MAC_ESP8266_H__
-#define __MAC_ESP8266_H__
+#ifndef __BITS_MAC_ESP8266_H__
+#define __BITS_MAC_ESP8266_H__
+
+#include "bits/socket.h"
 
 #define SOCKET_READABLE		0	/* Socket is reading bit. */
 #define SOCKET_WRITING		1	/* Socket is writing bit. */
@@ -9,5 +11,12 @@
 #define SET_BIT(r, b)	((r) |= (1 << (b)))
 #define CLR_BIT(r, b)	((r) &= ~(1 << (b)))
 #define ISBIT_SET(r, b)	(((r) & (1 << (b))) > 0)
+
+void InitESP8266(void);
+void BindTcpSocket(uint16_t port);
+SOCKET AcceptTcpSocket(void);
+ssize_t SendSocket(SOCKET s, void *buf, size_t len, int f);
+ssize_t RecvSocket(SOCKET s, void *buf, size_t len, int f);
+int ShutdownSocket(SOCKET s, int how);
 
 #endif
